4b06e0ad81137258f622cb7d190cab7fee5f2fea
Anton <toropow@gmail.com>
2020-10-24 17:21:26 +0300

    add example

--- homework2/main.py
+++ homework2/main.py
@@ -6,3 +6,26 @@ if __name__ == '__main__':
     print(yacht)
     loco = Locomotive(name="Tommy", age=25, experience=2, salary=10000, personal_car=False, fuel_level=10)
     print(loco)
+
+#Example work
+# This is Yacht
+# Color: White
+# Weight: 1000
+# Consumption per 100 km: 100
+# Type of engine: diesel
+# Capitan name: John
+# Capitan age:  30
+# Capitan experience 10,
+# Capitan star career at: 20
+#
+#
+# This is Locomotive
+# Color: Green
+# Weight: 2000
+# Consumption per 100 km: 300
+# Capacity of tank: 20000
+# Rain train sound: ChuhChuh... ToooTooo
+# Train manager name: Tommy
+# Train manager age:  25
+# Train manager experience 2,
+# Train manager star career at: 23
